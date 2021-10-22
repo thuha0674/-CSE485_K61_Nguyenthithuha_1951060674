dd6727b20051db43b402a68f9c79980251eb0a1e
thuha0674 <90234071+thuha0674@users.noreply.github.com>
2021-10-22 12:19:45 +0700

    Add files via upload

---
+++ exams.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 22, 2021 lúc 07:11 AM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `test_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `exams`
+--
+
+CREATE TABLE `exams` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `exam_title` varchar(100) NOT NULL,
+  `exam_datetime` date NOT NULL,
+  `duration` time NOT NULL,
+  `total_question` int(50) NOT NULL,
+  `marks_per_right_answer` int(30) NOT NULL,
+  `created_on` date NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `exam_code` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `exams`
+--
+
+INSERT INTO `exams` (`id`, `exam_title`, `exam_datetime`, `duration`, `total_question`, `marks_per_right_answer`, `created_on`, `status`, `exam_code`) VALUES
+(1, 'Công nghệ web', '2021-10-22', '10:11:41', 50, 2, '2021-10-22', 'started', '6');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `exams`
+--
+ALTER TABLE `exams`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
